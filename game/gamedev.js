9312e9baeffce24d81ce91b7b7cddfcee1eaae49
210815 <joshua@Maryanns-MacBook.local>
2016-06-08 08:45:05 +0800

    Game
    asdfghjkl

---
+++ game/gamedev.js
@@ -0,0 +1,11 @@
+
+$(".restart").hide()
+$(".jumpscare").hide()
+$(".gameover").hide()
+$( ".forest6Body" ).ready(function(){
+$(".gameover").fadein("slow");
+$(".jumpscare").fadein("fast");
+}
+//Your jquery goes here
+
+});
